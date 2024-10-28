4c3ca830c3b22f620113303ab75b928a31de8423
Alfredo Barrios <marode1994@gmail.com>
2024-10-28 14:17:25 -0500

    ok

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('portfolio.urls')),
+    path('blog/', include('blog.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
